31e65a234ba67db27b38f033a2fc33cab5e70f93
Aleksei Loos <lanbotdevman@gmail.com>
2022-06-20 20:22:22 +0300

    Initial commit

---
+++ src/main/java/ee/lbdm/lambda/authorizer/Application.java
@@ -0,0 +1,9 @@
+package ee.lbdm.lambda.authorizer;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
